f3fbcde7ef4c3e77d360b9dc689a0da2174bda8f
pachopascolat <patriciopascolat@gmail.com>
2020-03-22 11:43:29 -0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii_basic_api',
+    'username' => 'root',
+    'password' => 'jupit3r',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ migrations/m200322_143539_create_table_user.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+class m200322_143539_create_table_user extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string(),
+            'email' => $this->string()->notNull(),
+            'status' => $this->smallInteger()->notNull()->defaultValue('10'),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+            'verification_token' => $this->string(),
+            'access_token' => $this->string(),
+        ], $tableOptions);
+
+        $this->createIndex('email', '{{%user}}', 'email', true);
+        $this->createIndex('username', '{{%user}}', 'username', true);
+        $this->createIndex('password_reset_token', '{{%user}}', 'password_reset_token', true);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
